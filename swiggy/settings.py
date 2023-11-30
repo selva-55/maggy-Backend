ff30628da565ec951e80a067cf2287a6318639af
selva-55 <selvathamizh98@gmail.com>
2023-11-30 18:00:44 +0530

    data

--- swiggy/settings.py
+++ swiggy/settings.py
@@ -94,16 +94,16 @@ WSGI_APPLICATION = 'swiggy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'swiggy',
-        'USER': 'root',
-        'PASSWORD': 'Selva14@',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'swiggy',
+#         'USER': 'root',
+#         'PASSWORD': 'Selva14@',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
